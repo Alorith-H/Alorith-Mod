08fdf10d94d0446ffcafc47f27123f0e5530ed53
Alorith-H <2835147611@qq.com>
2025-05-19 17:15:48 +0800

    test

---
+++ src/main/java/com/alorith/AlorithClient.java
@@ -0,0 +1,10 @@
+package com.alorith;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AlorithClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
